26444fe18bdca8f11b1c799d9c8e305412f6c8a3
Daniel Standage <daniel.standage@gmail.com>
2020-10-15 19:06:08 -0400

    Add SRA data, update config

--- getdata.sh
+++ getdata.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-if [[ ! -f SRR5944233-R1.fastq ]]; then
+if [[ ! -f sample057-R1.fastq ]]; then
     if [[ ! -f  SRR5944233.1.sra ]]; then
+        echo "Downloading SRA file"
         curl -L https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos2/sra-pub-run-11/SRR5944233/SRR5944233.1 > SRR5944233.1.sra
     fi
+    echo "Converting SRA file to Fastq format"
     fastq-dump --split-files --defline-seq '@Sample057.$si/$ri' --defline-qual '+' SRR5944233.1.sra
     mv SRR5944233.1_1.fastq sample057-R1.fastq
     mv SRR5944233.1_2.fastq sample057-R2.fastq
